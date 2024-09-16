3a6ca2ad5c17d327910654c1351a4720457aa684
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2024-09-16 17:39:03 +0800

    element manipulation

---
+++ DOM/ElementManipulation/script.js
@@ -0,0 +1,19 @@
+// element.innerHTML -> update content
+const judul = document.getElementById('judul');
+judul.innerHTML = '<em>Tukar Tajuk</em>';
+
+// element.style.<propertyCSS>
+judul.style.color = 'lightblue';
+
+// element.setAttribute()
+const a = document.getElementsByTagName('a');
+    // a[0].setAttribute('id', 'link');
+
+// element.getAttribute()
+    // a[0].getAttribute('href');
+
+// element.removeAttribute()
+
+// element.classList()
+const p2 = document.querySelector('.p2');
+// p2.classList.add('label');
